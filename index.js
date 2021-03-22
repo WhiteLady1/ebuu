cd0c743024116a115507d51750119bada253269c
WhiteLady <megan15963@gmail.com>
2021-03-22 20:08:40 +0100

    Hotovo v HTML

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const arrow=document.querySelector(".arrow");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    arrow.style.display = "block";
+  } else {
+    arrow.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+arrow.addEventListener('click', topFunction)
